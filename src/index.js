683a833917c9d2213e988d47f904a6efd4555d21
AnastasiaFedotova <anastaisafedotova@gmail.com>
2020-02-24 13:32:28 +0300

    feat: add result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number) {
+   if (number < 0) {
+       let result = String(number).split("").reverse();
+       result.pop();
+       return result.join("");
+   }
+   else if (number < 10) return number;
+   else return Number(String(number).split("").reverse().join(""));
 }
